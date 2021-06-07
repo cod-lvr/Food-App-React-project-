6dba467df7ea2d5e0ea57663080c4d39e75cb30f
cod-lvr <aymang29@yahoo.com>
2021-06-08 01:42:36 +0200

    create a meals component

---
+++ src/Components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import AvailableMeals from "./AvailableMeal";
+import MealsSummary from "./MealsSummary";
+
+const Meals = props => {
+    return <Fragment>
+        <MealsSummary />
+        <AvailableMeals />
+    </Fragment>
+}
+
+export default Meals;
---
+++ src/Components/Meals/MealsSummary.js
@@ -0,0 +1,14 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Your everyday, right away</h2>
+      <p>
+      Order food and grocery delivery online from hundreds of restaurants and shops nearby.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
