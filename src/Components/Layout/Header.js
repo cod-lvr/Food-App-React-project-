ed4c13c70ee7d6e4015ac0a3dfc4ef73b547ec65
cod-lvr <aymang29@yahoo.com>
2021-06-07 05:50:59 +0200

    add app  src file

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,19 @@
+import classes from "./Header.module.css";
+import { Fragment } from "react";
+import HeaderCartButton from "../UI/HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>MyMeal</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
