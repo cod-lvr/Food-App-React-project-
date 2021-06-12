1f7b98706576309c771ad9260fef68bc12e5c455
cod-lvr <aymang29@yahoo.com>
2021-06-13 00:47:02 +0200

    modified UI folder

--- src/Components/UI/HeaderCartButton.js
+++ src/Components/UI/HeaderCartButton.js
@@ -1,17 +1,37 @@
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+    const [btnIsHighlight, setBtnHighLight] = useState(false);
     const cartCtx = useContext(CartContext);
+    const {items} = cartCtx;
 
     const numberOfCarItem = cartCtx.items.reduce((curr, item) => {
         return curr + item.amount;
     }, 0);
 
-    return <button className={classes.button} onClick={props.onClick}>
+    const btnClasses = `${classes.button} ${btnIsHighlight ? classes.bump : ""}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnHighLight(true);
+        
+       const timer = setTimeout(() => {
+            setBtnHighLight(false);
+        }, 300)
+
+        return () =>{
+            clearTimeout(timer);
+        };
+
+    }, [items]);
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
